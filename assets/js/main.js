1451e5fd42734218d5f599ea0340add43ad168ad
Mykhailo Shaban <palskifskiy@gmail.com>
2025-03-27 20:39:13 +0100

    NavBar Mobile/Desktop Upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-// Removed the code that generates the header
+
+  document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.details-toggle').forEach(button => {
+      button.addEventListener('click', () => {
+        const id = button.getAttribute('data-id');
+        const detailEl = document.getElementById(`${id}-details`);
+        
+        if (detailEl.classList.contains('hidden')) {
+          detailEl.classList.remove('hidden');
+        } else {
+          detailEl.classList.add('hidden');
+        }
+      });
+    });
+  });
